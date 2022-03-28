78a08a2ccc48b360a3d2743dc0e3eba1aa81f01d
mIhoom <mdmichalski.poczta@gmail.com>
2022-03-28 13:43:27 +0200

    zatwierdz

--- generator.py
+++ generator.py
@@ -1,17 +1,18 @@
 import random
 import string
+import sys
 
 while True:
     print('GENERATOR KLUCZY" \n'
-            'Generuje losowe klucze i umożliwia ich zapis do pliku.\n' 'Twój niepowtarzalny klucz to:  ')
+            'Generuje losowe klucze i umożliwia ich zapis do pliku.\n')
     
-#     list = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h','I', 'i', 'J', 'j', 'K', 'k',
-#             'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v',
-#             'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '!', '@', '#', '$', '%', '^', '&', '*', '?',]
-#     array = []
     length = 20
     password = ''.join(random.choices(string.ascii_letters+string.digits+string.punctuation,k=length,))
-    print(password)
+    print("Twój niepowtarzalny klucz to: ",password,' \n' "*Klucz zostanie zapisany do pliku .txt w miejscu lokalizacji programu"' \n')
 
-    if input('Chcesz kontunuować? t - Tak, generuj kolejny klucz / dowolny klawisz - Nie, zakończ program.\n' 'Wybór zatwierdź ENTEREM ') != 't':
+    if input('Chcesz kontunuować? \n' 't - Tak, generuj i zapisz kolejny klucz / dowolny klawisz - Nie, zakończ program.\n' 'Wybór zatwierdź ENTEREM ') != 't':
         break
+
+file_path = 'twojKlucz.txt'
+sys.stdout = open(file_path, "w")
+print("Twój niepowtarzalny klucz to: ",password)
